af2141188032411c0e5964233d8e5c3f86567cf4
oshadakavinda <oshadakavinda2@gmail.com>
2025-01-30 00:49:54 +0530

    new file:   GameStore.Api/Dtos/GameDetailsDto.cs 	renamed:    GameStore.Api/Dtos/GameDto.cs -> GameStore.Api/Dtos/GameSummaryDto.cs 	modified:   GameStore.Api/Dtos/UpdateGameDto.cs 	modified:   GameStore.Api/Endpoints/GamesEndpoints.cs 	modified:   GameStore.Api/GameStore.db 	modified:   GameStore.Api/GameStore.db-shm 	modified:   GameStore.Api/GameStore.db-wal 	modified:   GameStore.Api/Mapping/GameMapping.cs 	modified:   GameStore.Api/bin/Debug/net8.0/GameStore.Api.dll 	modified:   GameStore.Api/bin/Debug/net8.0/GameStore.Api.exe 	modified:   GameStore.Api/bin/Debug/net8.0/GameStore.Api.pdb 	modified:   GameStore.Api/games.http 	modified:   GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs 	modified:   GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfoInputs.cache 	modified:   GameStore.Api/obj/Debug/net8.0/GameStore.Api.csproj.CoreCompileInputs.cache 	modified:   GameStore.Api/obj/Debug/net8.0/GameStore.Api.dll 	modified:   GameStore.Api/obj/Debug/net8.0/GameStore.Api.genruntimeconfig.cache 	modified:   GameStore.Api/obj/Debug/net8.0/GameStore.Api.pdb 	modified:   GameStore.Api/obj/Debug/net8.0/GameStore.Api.sourcelink.json 	modified:   GameStore.Api/obj/Debug/net8.0/apphost.exe 	modified:   GameStore.Api/obj/Debug/net8.0/ref/GameStore.Api.dll 	modified:   GameStore.Api/obj/Debug/net8.0/refint/GameStore.Api.dll 	modified:   GameStore.Api/obj/GameStore.Api.csproj.nuget.dgspec.json 	modified:   GameStore.Api/obj/project.nuget.cache 	modified:   Readme.md

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -10,60 +10,59 @@ public static class GamesEndpoints
 {
     const string GetGameEndpointName = "GetGame";
 // Initialize a list of GameDto objects with sample game data
-private static readonly List<GameDto> games = [
-    // Each GameDto object represents a game with Id, Name, Genre, Price, and ReleaseDate
-    new (1, "Elden Ring", "Action RPG", 59.99m, new DateOnly(2022, 2, 25)),
-    new (2, "The Legend of Zelda", "Adventure", 49.99m, new DateOnly(2017, 3, 3)),
-    new (3, "Minecraft", "Sandbox", 26.95m, new DateOnly(2011, 11, 18))
-];
 
 public static RouteGroupBuilder MapGamesEndpoints (this WebApplication app)
 {
     var group = app.MapGroup("games");
-    // Define a GET endpoint at "games" that returns the list of games
-group.MapGet("/", () => games);
 
-//GET /games
-group.MapGet("/{id}", (int id) => 
-{
-    GameDto? game = games.Find(game =>game.Id == id);
+    // GET/gems
+    group.MapGet("/",  (GameStoreContext dbContext) => 
+             dbContext.Games
+                        .Include(game=>game.Genre)
+                        .Select(game => game.ToGameSummaryDto())
+                        .AsNoTracking()
+                     );
 
-    return game is null? Results.NotFound() : Results.Ok(game);
-})
-.WithName(GetGameEndpointName);
+
+
+//GET /games/id
+    group.MapGet("/{id}", async (int id, GameStoreContext dbContext) =>
+        {
+            Game? game = await dbContext.Games.FindAsync(id);
+
+            return game is null ? 
+                Results.NotFound() : Results.Ok(game.ToGameDetailsDto());
+        })
+        .WithName(GetGameEndpointName);
 
 group.MapPost("/",(CreateGameDto newGame,GameStoreContext dbContext)=>{
 
     Game game = newGame.ToEntity();
-    game.Genre = dbContext.Genres.Find(newGame.GenreId);
-
     
-
     dbContext.Games.Add(game);
     dbContext.SaveChanges();    
-
     
 
     return Results.CreatedAtRoute(
-        GetGameEndpointName,new {id = game.Id},game.ToDto());
+        GetGameEndpointName,
+        new {id = game.Id},
+        game.ToGameDetailsDto());
 }).WithParameterValidation()
 ;
 
 //PUT /games
-group.MapPut("/{id}",(int id, UpdateGameDto updatedGame) =>{
-    var index = games.FindIndex(game => game.Id == id);
-    if (index == -1)
-    {
-        return Results.NotFound();
-    }
-    games[index] = new GameDto(
-        id,
-        updatedGame.Name,
-        updatedGame.Genre,
-        updatedGame.Price,
-        updatedGame.ReleaseDate
-    );
+group.MapPut("/{id}",(int id, UpdateGameDto updatedGame ,GameStoreContext dbContext) =>{
+   var existingGame =  dbContext.Games.Find(id);
+    if (existingGame is null)
+            {
+                return Results.NotFound();
+            }
 
+    dbContext.Entry(existingGame)
+                .CurrentValues
+                .SetValues(updatedGame.ToEntity(id));
+
+    dbContext.SaveChanges();
     return Results.NoContent();
 });
 
--- GameStore.Api/Mapping/GameMapping.cs
+++ GameStore.Api/Mapping/GameMapping.cs
@@ -16,7 +16,19 @@ public static class GameMapping
         };
     }
 
-    public static GameDto ToDto(this Game game)
+    public static Game ToEntity(this UpdateGameDto game ,int id)
+    {
+        return new Game()
+        {
+            Id = id,
+            Name = game.Name,
+            GenreId = game.GenreId,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+    }
+
+    public static GameSummaryDto ToGameSummaryDto(this Game game)
     {
         return new(
             game.Id,
@@ -27,4 +39,14 @@ public static class GameMapping
     );
     }
 
+    public static GameDetailsDto ToGameDetailsDto(this Game game)
+    {
+        return new(
+            game.Id,
+            game.Name,
+            game.GenreId,
+            game.Price,
+            game.ReleaseDate
+    );
+    }
 }
